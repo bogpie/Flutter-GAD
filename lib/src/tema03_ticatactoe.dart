6c74ea65598ab5d06f3271b2d56dd8605cc6b677
Bogdan Piele <bogdanpielemail@gmail.com>
2020-12-04 01:26:31 +0200

    managed to treat each cell separately
    this is done by building the gridview with Tile objects which correspond to an index

--- lib/src/tema03_ticatactoe.dart
+++ lib/src/tema03_ticatactoe.dart
@@ -5,6 +5,46 @@ void main() {
   runApp(TicTacToe());
 }
 
+class Tile extends StatefulWidget {
+  Tile(this.index);
+
+  int index;
+  @override
+  _TileState createState() => _TileState(index);
+}
+
+class _TileState extends State<Tile> {
+
+  _TileState([this.index]);
+  int index;
+  int player = 0;
+  List<MaterialColor> colors = [Colors.red, Colors.green];
+  MaterialColor color;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('Clicked $index');
+        setState(() {
+          color = colors[player];
+          if (player == 0)
+            player = 1;
+          else
+            player = 0;
+        });
+      },
+      child: AnimatedContainer(
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.4),
+          color: color,
+        ),
+        duration: const Duration(milliseconds: 250),
+      ),
+    );
+  }
+}
+
 class TicTacToe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -18,9 +58,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int player = 0;
-  List<MaterialColor> colors = [Colors.red, Colors.green];
-  MaterialColor color;
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,27 +74,9 @@ class _HomePageState extends State<HomePage> {
           itemCount: 9,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3, crossAxisSpacing: 0, mainAxisSpacing: 0),
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                print('Clicked $index');
-                setState(() {
-                  color = colors[player];
-                  if (player == 0)
-                    player = 1;
-                  else
-                    player = 0;
-                });
-              },
-              child: AnimatedContainer(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 0.4),
-                  color: color,
-                ),
-                duration: const Duration(milliseconds: 250),
-              ),
-            );
-          },
+
+          itemBuilder: (BuildContext context, int index) => Tile(index),
+
         ));
   }
 }
