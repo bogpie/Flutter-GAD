872ad254acd97848013f70ffdb747d5d7f7cff77
Bogdan Piele <bogdanpielemail@gmail.com>
2020-12-08 18:44:55 +0200

    deleted unnecessary comment

--- lib/src/tema02_guess_number.dart
+++ lib/src/tema02_guess_number.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
-//import 'package:validators/validators.dart'; //actually isNumeric() doesn't support rational numbers
 
 void main() {
   runApp(Tema02App());
