6ab28fd6354ff4c006e023bea7b2ba98d1d3fc69
Bogdan Piele <bogdanpielemail@gmail.com>
2020-12-14 16:17:41 +0200

    trying to fix CI/CD

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,14 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:tema1/main.dart';
-import 'package:tema1/src/tema01.dart';
+import 'package:tema1/src/tema04_movie_list.dart' as app;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Tema01App());
+    await tester.pumpWidget(app.MovieListApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
