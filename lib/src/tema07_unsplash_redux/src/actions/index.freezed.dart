2c17a4f4b3f9cfc0a67cbaf8b5de50f0d2818e65
Bogdan Piele <bogdanpielemail@gmail.com>
2021-01-05 17:25:42 +0200

    reformatted

--- lib/src/tema07_unsplash_redux/src/actions/index.freezed.dart
+++ lib/src/tema07_unsplash_redux/src/actions/index.freezed.dart
@@ -45,6 +45,7 @@ mixin _$GetPhotos {
     @required Result successful(List<Photo> photos),
     @required Result error(dynamic error),
   });
+
   @optionalTypeArgs
   Result maybeWhen<Result extends Object>(
     Result $default(), {
@@ -52,12 +53,14 @@ mixin _$GetPhotos {
     Result error(dynamic error),
     @required Result orElse(),
   });
+
   @optionalTypeArgs
   Result map<Result extends Object>(
     Result $default(GetPhotosStart value), {
     @required Result successful(GetPhotosSuccessful value),
     @required Result error(GetPhotosError value),
   });
+
   @optionalTypeArgs
   Result maybeMap<Result extends Object>(
     Result $default(GetPhotosStart value), {
@@ -69,8 +72,7 @@ mixin _$GetPhotos {
 
 /// @nodoc
 abstract class $GetPhotosCopyWith<$Res> {
-  factory $GetPhotosCopyWith(GetPhotos value, $Res Function(GetPhotos) then) =
-      _$GetPhotosCopyWithImpl<$Res>;
+  factory $GetPhotosCopyWith(GetPhotos value, $Res Function(GetPhotos) then) = _$GetPhotosCopyWithImpl<$Res>;
 }
 
 /// @nodoc
@@ -78,22 +80,21 @@ class _$GetPhotosCopyWithImpl<$Res> implements $GetPhotosCopyWith<$Res> {
   _$GetPhotosCopyWithImpl(this._value, this._then);
 
   final GetPhotos _value;
+
   // ignore: unused_field
   final $Res Function(GetPhotos) _then;
 }
 
 /// @nodoc
 abstract class $GetPhotosStartCopyWith<$Res> {
-  factory $GetPhotosStartCopyWith(
-          GetPhotosStart value, $Res Function(GetPhotosStart) then) =
+  factory $GetPhotosStartCopyWith(GetPhotosStart value, $Res Function(GetPhotosStart) then) =
       _$GetPhotosStartCopyWithImpl<$Res>;
 }
 
 /// @nodoc
 class _$GetPhotosStartCopyWithImpl<$Res> extends _$GetPhotosCopyWithImpl<$Res>
     implements $GetPhotosStartCopyWith<$Res> {
-  _$GetPhotosStartCopyWithImpl(
-      GetPhotosStart _value, $Res Function(GetPhotosStart) _then)
+  _$GetPhotosStartCopyWithImpl(GetPhotosStart _value, $Res Function(GetPhotosStart) _then)
       : super(_value, (v) => _then(v as GetPhotosStart));
 
   @override
@@ -180,18 +181,16 @@ abstract class GetPhotosStart implements GetPhotos {
 
 /// @nodoc
 abstract class $GetPhotosSuccessfulCopyWith<$Res> {
-  factory $GetPhotosSuccessfulCopyWith(
-          GetPhotosSuccessful value, $Res Function(GetPhotosSuccessful) then) =
+  factory $GetPhotosSuccessfulCopyWith(GetPhotosSuccessful value, $Res Function(GetPhotosSuccessful) then) =
       _$GetPhotosSuccessfulCopyWithImpl<$Res>;
+
   $Res call({List<Photo> photos});
 }
 
 /// @nodoc
-class _$GetPhotosSuccessfulCopyWithImpl<$Res>
-    extends _$GetPhotosCopyWithImpl<$Res>
+class _$GetPhotosSuccessfulCopyWithImpl<$Res> extends _$GetPhotosCopyWithImpl<$Res>
     implements $GetPhotosSuccessfulCopyWith<$Res> {
-  _$GetPhotosSuccessfulCopyWithImpl(
-      GetPhotosSuccessful _value, $Res Function(GetPhotosSuccessful) _then)
+  _$GetPhotosSuccessfulCopyWithImpl(GetPhotosSuccessful _value, $Res Function(GetPhotosSuccessful) _then)
       : super(_value, (v) => _then(v as GetPhotosSuccessful));
 
   @override
@@ -223,13 +222,11 @@ class _$GetPhotosSuccessful implements GetPhotosSuccessful {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is GetPhotosSuccessful &&
-            (identical(other.photos, photos) ||
-                const DeepCollectionEquality().equals(other.photos, photos)));
+            (identical(other.photos, photos) || const DeepCollectionEquality().equals(other.photos, photos)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(photos);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(photos);
 
   @override
   $GetPhotosSuccessfulCopyWith<GetPhotosSuccessful> get copyWith =>
@@ -296,22 +293,22 @@ abstract class GetPhotosSuccessful implements GetPhotos {
   const factory GetPhotosSuccessful(List<Photo> photos) = _$GetPhotosSuccessful;
 
   List<Photo> get photos;
+
   $GetPhotosSuccessfulCopyWith<GetPhotosSuccessful> get copyWith;
 }
 
 /// @nodoc
 abstract class $GetPhotosErrorCopyWith<$Res> {
-  factory $GetPhotosErrorCopyWith(
-          GetPhotosError value, $Res Function(GetPhotosError) then) =
+  factory $GetPhotosErrorCopyWith(GetPhotosError value, $Res Function(GetPhotosError) then) =
       _$GetPhotosErrorCopyWithImpl<$Res>;
+
   $Res call({dynamic error});
 }
 
 /// @nodoc
 class _$GetPhotosErrorCopyWithImpl<$Res> extends _$GetPhotosCopyWithImpl<$Res>
     implements $GetPhotosErrorCopyWith<$Res> {
-  _$GetPhotosErrorCopyWithImpl(
-      GetPhotosError _value, $Res Function(GetPhotosError) _then)
+  _$GetPhotosErrorCopyWithImpl(GetPhotosError _value, $Res Function(GetPhotosError) _then)
       : super(_value, (v) => _then(v as GetPhotosError));
 
   @override
@@ -343,13 +340,11 @@ class _$GetPhotosError implements GetPhotosError {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is GetPhotosError &&
-            (identical(other.error, error) ||
-                const DeepCollectionEquality().equals(other.error, error)));
+            (identical(other.error, error) || const DeepCollectionEquality().equals(other.error, error)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(error);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(error);
 
   @override
   $GetPhotosErrorCopyWith<GetPhotosError> get copyWith =>
@@ -416,6 +411,7 @@ abstract class GetPhotosError implements GetPhotos {
   const factory GetPhotosError(dynamic error) = _$GetPhotosError;
 
   dynamic get error;
+
   $GetPhotosErrorCopyWith<GetPhotosError> get copyWith;
 }
 
@@ -444,18 +440,18 @@ mixin _$SetSelectedPhoto {
 
 /// @nodoc
 abstract class $SetSelectedPhotoCopyWith<$Res> {
-  factory $SetSelectedPhotoCopyWith(
-          SetSelectedPhoto value, $Res Function(SetSelectedPhoto) then) =
+  factory $SetSelectedPhotoCopyWith(SetSelectedPhoto value, $Res Function(SetSelectedPhoto) then) =
       _$SetSelectedPhotoCopyWithImpl<$Res>;
+
   $Res call({String photoId});
 }
 
 /// @nodoc
-class _$SetSelectedPhotoCopyWithImpl<$Res>
-    implements $SetSelectedPhotoCopyWith<$Res> {
+class _$SetSelectedPhotoCopyWithImpl<$Res> implements $SetSelectedPhotoCopyWith<$Res> {
   _$SetSelectedPhotoCopyWithImpl(this._value, this._then);
 
   final SetSelectedPhoto _value;
+
   // ignore: unused_field
   final $Res Function(SetSelectedPhoto) _then;
 
@@ -470,21 +466,18 @@ class _$SetSelectedPhotoCopyWithImpl<$Res>
 }
 
 /// @nodoc
-abstract class $SetSelectedPhoto$CopyWith<$Res>
-    implements $SetSelectedPhotoCopyWith<$Res> {
-  factory $SetSelectedPhoto$CopyWith(
-          SetSelectedPhoto$ value, $Res Function(SetSelectedPhoto$) then) =
+abstract class $SetSelectedPhoto$CopyWith<$Res> implements $SetSelectedPhotoCopyWith<$Res> {
+  factory $SetSelectedPhoto$CopyWith(SetSelectedPhoto$ value, $Res Function(SetSelectedPhoto$) then) =
       _$SetSelectedPhoto$CopyWithImpl<$Res>;
+
   @override
   $Res call({String photoId});
 }
 
 /// @nodoc
-class _$SetSelectedPhoto$CopyWithImpl<$Res>
-    extends _$SetSelectedPhotoCopyWithImpl<$Res>
+class _$SetSelectedPhoto$CopyWithImpl<$Res> extends _$SetSelectedPhotoCopyWithImpl<$Res>
     implements $SetSelectedPhoto$CopyWith<$Res> {
-  _$SetSelectedPhoto$CopyWithImpl(
-      SetSelectedPhoto$ _value, $Res Function(SetSelectedPhoto$) _then)
+  _$SetSelectedPhoto$CopyWithImpl(SetSelectedPhoto$ _value, $Res Function(SetSelectedPhoto$) _then)
       : super(_value, (v) => _then(v as SetSelectedPhoto$));
 
   @override
@@ -516,13 +509,11 @@ class _$SetSelectedPhoto$ implements SetSelectedPhoto$ {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is SetSelectedPhoto$ &&
-            (identical(other.photoId, photoId) ||
-                const DeepCollectionEquality().equals(other.photoId, photoId)));
+            (identical(other.photoId, photoId) || const DeepCollectionEquality().equals(other.photoId, photoId)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(photoId);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(photoId);
 
   @override
   $SetSelectedPhoto$CopyWith<SetSelectedPhoto$> get copyWith =>
@@ -534,6 +525,7 @@ abstract class SetSelectedPhoto$ implements SetSelectedPhoto {
 
   @override
   String get photoId;
+
   @override
   $SetSelectedPhoto$CopyWith<SetSelectedPhoto$> get copyWith;
 }
@@ -563,9 +555,8 @@ mixin _$UpdateColor {
 
 /// @nodoc
 abstract class $UpdateColorCopyWith<$Res> {
-  factory $UpdateColorCopyWith(
-          UpdateColor value, $Res Function(UpdateColor) then) =
-      _$UpdateColorCopyWithImpl<$Res>;
+  factory $UpdateColorCopyWith(UpdateColor value, $Res Function(UpdateColor) then) = _$UpdateColorCopyWithImpl<$Res>;
+
   $Res call({String color});
 }
 
@@ -574,6 +565,7 @@ class _$UpdateColorCopyWithImpl<$Res> implements $UpdateColorCopyWith<$Res> {
   _$UpdateColorCopyWithImpl(this._value, this._then);
 
   final UpdateColor _value;
+
   // ignore: unused_field
   final $Res Function(UpdateColor) _then;
 
@@ -588,20 +580,17 @@ class _$UpdateColorCopyWithImpl<$Res> implements $UpdateColorCopyWith<$Res> {
 }
 
 /// @nodoc
-abstract class _$UpdateColorCopyWith<$Res>
-    implements $UpdateColorCopyWith<$Res> {
-  factory _$UpdateColorCopyWith(
-          _UpdateColor value, $Res Function(_UpdateColor) then) =
+abstract class _$UpdateColorCopyWith<$Res> implements $UpdateColorCopyWith<$Res> {
+  factory _$UpdateColorCopyWith(_UpdateColor value, $Res Function(_UpdateColor) then) =
       __$UpdateColorCopyWithImpl<$Res>;
+
   @override
   $Res call({String color});
 }
 
 /// @nodoc
-class __$UpdateColorCopyWithImpl<$Res> extends _$UpdateColorCopyWithImpl<$Res>
-    implements _$UpdateColorCopyWith<$Res> {
-  __$UpdateColorCopyWithImpl(
-      _UpdateColor _value, $Res Function(_UpdateColor) _then)
+class __$UpdateColorCopyWithImpl<$Res> extends _$UpdateColorCopyWithImpl<$Res> implements _$UpdateColorCopyWith<$Res> {
+  __$UpdateColorCopyWithImpl(_UpdateColor _value, $Res Function(_UpdateColor) _then)
       : super(_value, (v) => _then(v as _UpdateColor));
 
   @override
@@ -633,17 +622,14 @@ class _$_UpdateColor implements _UpdateColor {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _UpdateColor &&
-            (identical(other.color, color) ||
-                const DeepCollectionEquality().equals(other.color, color)));
+            (identical(other.color, color) || const DeepCollectionEquality().equals(other.color, color)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(color);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(color);
 
   @override
-  _$UpdateColorCopyWith<_UpdateColor> get copyWith =>
-      __$UpdateColorCopyWithImpl<_UpdateColor>(this, _$identity);
+  _$UpdateColorCopyWith<_UpdateColor> get copyWith => __$UpdateColorCopyWithImpl<_UpdateColor>(this, _$identity);
 }
 
 abstract class _UpdateColor implements UpdateColor {
@@ -651,6 +637,7 @@ abstract class _UpdateColor implements UpdateColor {
 
   @override
   String get color;
+
   @override
   _$UpdateColorCopyWith<_UpdateColor> get copyWith;
 }
@@ -680,9 +667,8 @@ mixin _$UpdateField {
 
 /// @nodoc
 abstract class $UpdateFieldCopyWith<$Res> {
-  factory $UpdateFieldCopyWith(
-          UpdateField value, $Res Function(UpdateField) then) =
-      _$UpdateFieldCopyWithImpl<$Res>;
+  factory $UpdateFieldCopyWith(UpdateField value, $Res Function(UpdateField) then) = _$UpdateFieldCopyWithImpl<$Res>;
+
   $Res call({String field});
 }
 
@@ -691,6 +677,7 @@ class _$UpdateFieldCopyWithImpl<$Res> implements $UpdateFieldCopyWith<$Res> {
   _$UpdateFieldCopyWithImpl(this._value, this._then);
 
   final UpdateField _value;
+
   // ignore: unused_field
   final $Res Function(UpdateField) _then;
 
@@ -705,20 +692,17 @@ class _$UpdateFieldCopyWithImpl<$Res> implements $UpdateFieldCopyWith<$Res> {
 }
 
 /// @nodoc
-abstract class _$UpdateFieldCopyWith<$Res>
-    implements $UpdateFieldCopyWith<$Res> {
-  factory _$UpdateFieldCopyWith(
-          _UpdateField value, $Res Function(_UpdateField) then) =
+abstract class _$UpdateFieldCopyWith<$Res> implements $UpdateFieldCopyWith<$Res> {
+  factory _$UpdateFieldCopyWith(_UpdateField value, $Res Function(_UpdateField) then) =
       __$UpdateFieldCopyWithImpl<$Res>;
+
   @override
   $Res call({String field});
 }
 
 /// @nodoc
-class __$UpdateFieldCopyWithImpl<$Res> extends _$UpdateFieldCopyWithImpl<$Res>
-    implements _$UpdateFieldCopyWith<$Res> {
-  __$UpdateFieldCopyWithImpl(
-      _UpdateField _value, $Res Function(_UpdateField) _then)
+class __$UpdateFieldCopyWithImpl<$Res> extends _$UpdateFieldCopyWithImpl<$Res> implements _$UpdateFieldCopyWith<$Res> {
+  __$UpdateFieldCopyWithImpl(_UpdateField _value, $Res Function(_UpdateField) _then)
       : super(_value, (v) => _then(v as _UpdateField));
 
   @override
@@ -750,17 +734,14 @@ class _$_UpdateField implements _UpdateField {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _UpdateField &&
-            (identical(other.field, field) ||
-                const DeepCollectionEquality().equals(other.field, field)));
+            (identical(other.field, field) || const DeepCollectionEquality().equals(other.field, field)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(field);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(field);
 
   @override
-  _$UpdateFieldCopyWith<_UpdateField> get copyWith =>
-      __$UpdateFieldCopyWithImpl<_UpdateField>(this, _$identity);
+  _$UpdateFieldCopyWith<_UpdateField> get copyWith => __$UpdateFieldCopyWithImpl<_UpdateField>(this, _$identity);
 }
 
 abstract class _UpdateField implements UpdateField {
@@ -768,6 +749,7 @@ abstract class _UpdateField implements UpdateField {
 
   @override
   String get field;
+
   @override
   _$UpdateFieldCopyWith<_UpdateField> get copyWith;
 }
@@ -797,18 +779,18 @@ mixin _$UpdateOrderBy {
 
 /// @nodoc
 abstract class $UpdateOrderByCopyWith<$Res> {
-  factory $UpdateOrderByCopyWith(
-          UpdateOrderBy value, $Res Function(UpdateOrderBy) then) =
+  factory $UpdateOrderByCopyWith(UpdateOrderBy value, $Res Function(UpdateOrderBy) then) =
       _$UpdateOrderByCopyWithImpl<$Res>;
+
   $Res call({String orderBy});
 }
 
 /// @nodoc
-class _$UpdateOrderByCopyWithImpl<$Res>
-    implements $UpdateOrderByCopyWith<$Res> {
+class _$UpdateOrderByCopyWithImpl<$Res> implements $UpdateOrderByCopyWith<$Res> {
   _$UpdateOrderByCopyWithImpl(this._value, this._then);
 
   final UpdateOrderBy _value;
+
   // ignore: unused_field
   final $Res Function(UpdateOrderBy) _then;
 
@@ -823,21 +805,18 @@ class _$UpdateOrderByCopyWithImpl<$Res>
 }
 
 /// @nodoc
-abstract class _$UpdateOrderByCopyWith<$Res>
-    implements $UpdateOrderByCopyWith<$Res> {
-  factory _$UpdateOrderByCopyWith(
-          _UpdateOrderBy value, $Res Function(_UpdateOrderBy) then) =
+abstract class _$UpdateOrderByCopyWith<$Res> implements $UpdateOrderByCopyWith<$Res> {
+  factory _$UpdateOrderByCopyWith(_UpdateOrderBy value, $Res Function(_UpdateOrderBy) then) =
       __$UpdateOrderByCopyWithImpl<$Res>;
+
   @override
   $Res call({String orderBy});
 }
 
 /// @nodoc
-class __$UpdateOrderByCopyWithImpl<$Res>
-    extends _$UpdateOrderByCopyWithImpl<$Res>
+class __$UpdateOrderByCopyWithImpl<$Res> extends _$UpdateOrderByCopyWithImpl<$Res>
     implements _$UpdateOrderByCopyWith<$Res> {
-  __$UpdateOrderByCopyWithImpl(
-      _UpdateOrderBy _value, $Res Function(_UpdateOrderBy) _then)
+  __$UpdateOrderByCopyWithImpl(_UpdateOrderBy _value, $Res Function(_UpdateOrderBy) _then)
       : super(_value, (v) => _then(v as _UpdateOrderBy));
 
   @override
@@ -869,13 +848,11 @@ class _$_UpdateOrderBy implements _UpdateOrderBy {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _UpdateOrderBy &&
-            (identical(other.orderBy, orderBy) ||
-                const DeepCollectionEquality().equals(other.orderBy, orderBy)));
+            (identical(other.orderBy, orderBy) || const DeepCollectionEquality().equals(other.orderBy, orderBy)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(orderBy);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(orderBy);
 
   @override
   _$UpdateOrderByCopyWith<_UpdateOrderBy> get copyWith =>
@@ -887,6 +864,7 @@ abstract class _UpdateOrderBy implements UpdateOrderBy {
 
   @override
   String get orderBy;
+
   @override
   _$UpdateOrderByCopyWith<_UpdateOrderBy> get copyWith;
 }
@@ -916,9 +894,8 @@ mixin _$UpdateQuery {
 
 /// @nodoc
 abstract class $UpdateQueryCopyWith<$Res> {
-  factory $UpdateQueryCopyWith(
-          UpdateQuery value, $Res Function(UpdateQuery) then) =
-      _$UpdateQueryCopyWithImpl<$Res>;
+  factory $UpdateQueryCopyWith(UpdateQuery value, $Res Function(UpdateQuery) then) = _$UpdateQueryCopyWithImpl<$Res>;
+
   $Res call({String query});
 }
 
@@ -927,6 +904,7 @@ class _$UpdateQueryCopyWithImpl<$Res> implements $UpdateQueryCopyWith<$Res> {
   _$UpdateQueryCopyWithImpl(this._value, this._then);
 
   final UpdateQuery _value;
+
   // ignore: unused_field
   final $Res Function(UpdateQuery) _then;
 
@@ -941,20 +919,17 @@ class _$UpdateQueryCopyWithImpl<$Res> implements $UpdateQueryCopyWith<$Res> {
 }
 
 /// @nodoc
-abstract class _$UpdateQueryCopyWith<$Res>
-    implements $UpdateQueryCopyWith<$Res> {
-  factory _$UpdateQueryCopyWith(
-          _UpdateQuery value, $Res Function(_UpdateQuery) then) =
+abstract class _$UpdateQueryCopyWith<$Res> implements $UpdateQueryCopyWith<$Res> {
+  factory _$UpdateQueryCopyWith(_UpdateQuery value, $Res Function(_UpdateQuery) then) =
       __$UpdateQueryCopyWithImpl<$Res>;
+
   @override
   $Res call({String query});
 }
 
 /// @nodoc
-class __$UpdateQueryCopyWithImpl<$Res> extends _$UpdateQueryCopyWithImpl<$Res>
-    implements _$UpdateQueryCopyWith<$Res> {
-  __$UpdateQueryCopyWithImpl(
-      _UpdateQuery _value, $Res Function(_UpdateQuery) _then)
+class __$UpdateQueryCopyWithImpl<$Res> extends _$UpdateQueryCopyWithImpl<$Res> implements _$UpdateQueryCopyWith<$Res> {
+  __$UpdateQueryCopyWithImpl(_UpdateQuery _value, $Res Function(_UpdateQuery) _then)
       : super(_value, (v) => _then(v as _UpdateQuery));
 
   @override
@@ -986,17 +961,14 @@ class _$_UpdateQuery implements _UpdateQuery {
   bool operator ==(dynamic other) {
     return identical(this, other) ||
         (other is _UpdateQuery &&
-            (identical(other.query, query) ||
-                const DeepCollectionEquality().equals(other.query, query)));
+            (identical(other.query, query) || const DeepCollectionEquality().equals(other.query, query)));
   }
 
   @override
-  int get hashCode =>
-      runtimeType.hashCode ^ const DeepCollectionEquality().hash(query);
+  int get hashCode => runtimeType.hashCode ^ const DeepCollectionEquality().hash(query);
 
   @override
-  _$UpdateQueryCopyWith<_UpdateQuery> get copyWith =>
-      __$UpdateQueryCopyWithImpl<_UpdateQuery>(this, _$identity);
+  _$UpdateQueryCopyWith<_UpdateQuery> get copyWith => __$UpdateQueryCopyWithImpl<_UpdateQuery>(this, _$identity);
 }
 
 abstract class _UpdateQuery implements UpdateQuery {
@@ -1004,6 +976,7 @@ abstract class _UpdateQuery implements UpdateQuery {
 
   @override
   String get query;
+
   @override
   _$UpdateQueryCopyWith<_UpdateQuery> get copyWith;
 }
